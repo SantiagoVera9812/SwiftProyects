3fda84b1c6b0051ecf18d6963711f46b66aecb6b
Christian Santiago Vera Rojas <christian.vera@globant.com>
2024-10-01 09:48:00 -0500

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Christian Santiago Vera Rojas on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GoodWeatherTests/GoodWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoodWeatherTests.swift
+//  GoodWeatherTests
+//
+//  Created by Christian Santiago Vera Rojas on 1/10/24.
+//
+
+import Testing
+@testable import GoodWeather
+
+struct GoodWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
